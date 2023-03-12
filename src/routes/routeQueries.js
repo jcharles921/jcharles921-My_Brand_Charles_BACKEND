611b112fd45529d8b429b511ef7c7ee13f2810c4
ykut <ykut@kali.zd>
2023-03-12 09:54:42 +0200

    trying before demo

--- src/routes/routeQueries.js
+++ src/routes/routeQueries.js
@@ -2,7 +2,7 @@ import express from "express";
 import Queries from "../controllers/Queries.js";
 import isProtected from "../middleware/isProtected.js";
 const router= express.Router();
-router.post("/",isProtected, Queries.createQuery);
+router.post("/", Queries.createQuery);
 router.get("/",isProtected,  Queries.getAllQueries );
 
 
